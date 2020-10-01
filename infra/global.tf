f208b3b63b67b7582325526c1de7138f74760319
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2020-10-01 10:32:32 +0200

    feat(infra/iac/state): manage remote state backend
    add remote state backend and locking with aws s3 and aws dynamodb
    
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- infra/global.tf
+++ infra/global.tf
@@ -1,8 +1,25 @@
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+
+module "terraform_state_backend" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=0.26.0"
+  namespace  = var.app_namespace
+  stage      = var.app_stage
+  name       = var.app_name
+  attributes = ["state"]
+
+  terraform_backend_config_file_path = var.terraform_state_backend_config_file_path
+  terraform_backend_config_file_name = var.terraform_state_backend_config_file_name
+  force_destroy                      = var.terraform_state_backend_force_destroy
+}
+
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=0.5.0"
-  namespace  = "falco"
-  stage      = "demo"
-  name       = "test-infra"
+  namespace  = var.app_namespace
+  stage      = var.app_stage
+  name       = var.app_name
   attributes = []
   delimiter  = "-"
 
