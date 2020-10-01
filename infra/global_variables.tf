434851b88392b6357449bc792031c7e3c428c502
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2020-10-01 10:32:32 +0200

    feat(infra/iac/state): manage remote state backend
    add remote state backend and locking with aws s3 and aws dynamodb
    
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- infra/global_variables.tf
+++ infra/global_variables.tf
@@ -1,4 +1,26 @@
+variable "terraform_state_backend_force_destroy" {
+  type = bool
+  default = false
+  description = "Whether to allow the S3 remote state backend to be deleted. Useful when migrating the state and/or destroying all the resources."
+}
+
+variable "terraform_state_backend_config_file_path" {
+  default = "."
+  description = "Directory for the terraform backend config file."
+}
+
+variable "terraform_state_backend_config_file_name" {
+  default = "terraform_backend.tf"
+  description = "The file name for the terraform backend config file."
+}
+
 variable "region" {
   default     = "us-east-2"
   description = "AWS region"
 }
+
+variable "app_name" {}
+
+variable "app_namespace" {}
+
+variable "app_stage" {}
