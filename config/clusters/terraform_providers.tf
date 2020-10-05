c3d6fa4a279d82676cc13475627136e80ad93221
jonahjon <jonahjones094@gmail.com>
2020-10-05 10:56:31 -0400

    adding updated scripts/terraform
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- config/clusters/terraform_providers.tf
+++ config/clusters/terraform_providers.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  version = ">= 2.28.1"
-  region  = "us-east-2"
+  version                 = ">= 2.28.1"
+  region                  = "us-west-2"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "default"
 }
 
 provider "kubernetes" {
