97657601d13976d9b61a6b9d29bfde97982a793a
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2020-10-21 11:07:36 +0200

    feat(1password.sh): add gpg verify on op cli binary
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- 1password.sh
+++ 1password.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if type op &> /dev/null; then
   echo "Onepassword CLI exists"
   op --version
@@ -8,8 +10,16 @@ else
     case $(uname | tr '[:upper:]' '[:lower:]') in
     linux*)
         mkdir temp
-        curl -s https://cache.agilebits.com/dist/1P/op/pkg/v1.7.0/op_linux_386_v1.7.0.zip -o temp/op.zip
+        curl -s \
+          https://cache.agilebits.com/dist/1P/op/pkg/v1.7.0/op_linux_386_v1.7.0.zip \
+          -o temp/op.zip
         unzip temp/op.zip -d temp/
+        gpg --receive-keys \
+          --keyserver pool.sks-keyservers.net \
+          3FEF9748469ADBE15DA7CA80AC2D62742012EA22
+        gpg --verify \
+          temp/op.sig \
+          temp/op
         mv temp/op /usr/local/bin
         rm -rf temp/
         ;;
