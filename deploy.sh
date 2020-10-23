7209ed646490bec824c83cae9c42c8f5bc7df95d
jonahjon <jonahjones094@gmail.com>
2020-10-23 11:09:01 -0400

    trying terraform fix
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ function terraform-install() {
     sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n |
     tail -n1)
   curl ${LATEST_URL} > /tmp/terraform.zip
-  cd tmp/
+  cd /tmp
   unzip terraform.zip
   rm -rf terraform.zip
   install terraform /usr/local/bin/
