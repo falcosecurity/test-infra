33f34ddea6633c3fb68d5bbabfe857d8b72d6601
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2020-10-24 19:40:42 +0200

    refactor(config/clusters): remove state backend mgmt from cluster infra code
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/global_variables.tf
+++ config/clusters/global_variables.tf
@@ -1,19 +1,3 @@
-variable "terraform_state_backend_force_destroy" {
-  type        = bool
-  default     = false
-  description = "Whether to allow the S3 remote state backend to be deleted. Useful when migrating the state and/or destroying all the resources."
-}
-
-variable "terraform_state_backend_config_file_path" {
-  default     = "."
-  description = "Directory for the terraform backend config file."
-}
-
-variable "terraform_state_backend_config_file_name" {
-  default     = "terraform_backend.tf"
-  description = "The file name for the terraform backend config file."
-}
-
 variable "region" {
   default     = "eu-west-1"
   description = "AWS region"
