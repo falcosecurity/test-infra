1179fe09d7b73198fc256213aa2a1415ee41697a
jonahjon <jonahjones094@gmail.com>
2020-11-03 10:55:01 -0500

    adding 2nd circleci job for prow to seperate from TF steps
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- tools/deploy_terraform.sh
+++ tools/deploy_terraform.sh
@@ -14,11 +14,6 @@ function main() {
   terraform-install
   echo "Running Terraform" 
   createCluster
-  echo "Launching Configmaps, and prereq software" 
-  launchConfig
-  # echo "Launching Prow microservices" 
-  # launchProw
-  # echo "All done!"
 }
 
 function terraform-install() {
@@ -53,27 +48,6 @@ function createCluster() {
   terraform validate config/clusters
   terraform apply -var-file config/clusters/prow.tfvars -auto-approve config/clusters
   aws eks --region ${ZONE} update-kubeconfig --name falco-prow-test-infra
-}
-
-function launchConfig(){
-  #ALB Ingress and ebs CSI driver
-  kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master"
-
-  kubectl create configmap plugins --from-file=plugins.yaml=./config/plugins.yaml || true
-  kubectl create configmap config --from-file "./config/config.yaml" || true
-  kubectl create configmap job-config --from-file "./config/jobs/config.yaml" || true
-  kubectl create configmap branding --from-file "./config/branding" || true
-  kubectl create secret generic s3-credentials --from-literal=service-account.json="$(./tools/1password.sh -d config/prow/service-account.json)" || true
-
-  #Github related items
-  kubectl create secret generic hmac-token --from-literal=hmac="$(./tools/1password.sh -d config/prow/hmac-token)" || true
-  kubectl create secret generic oauth-token --from-literal=oauth="$(./tools/1password.sh -d config/prow/oauth-token)" || true
-  kubectl create secret generic github-oauth-config --from-file=secret=./config/prow/github_oauth || true
-  kubectl create secret generic cookie --from-file=secret=./config/prow/cookie.txt || true
-}
-
-function launchProw(){
-  kubectl apply -f config/prow/
 }
 
 function cleanup() {
