551ed47a8ad1ae013b006f8c956681f9b4303275
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2020-11-05 19:36:26 +0100

    feat(config/clusters/kubernetes/deck): add acm certificate for deck lb svc https listener
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/kubernetes.tf
+++ config/clusters/kubernetes.tf
@@ -1,1 +1,30 @@
+data "kubernetes_service" "deck" {
+  metadata {
+    name      = "deck"
+    namespace = "default"
+  }
+}
 
+#data "aws_lb" "deck" {
+#  name = data.kubernetes_service.deck.load_balancer_ingress[0].hostname
+#}
+
+#data "aws_lb_listener" "deck" {
+#  load_balancer_arn = data.aws_lb.deck.arn
+#  port              = 9090
+#}
+
+resource "aws_acm_certificate" "deck" {
+  domain_name       = "prow.falco.org"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#resource "aws_lb_listener_certificate" "deck" {
+#  #listener_arn    = data.aws_lb_listener.deck.arn
+#  listener_arn    = ""
+#  certificate_arn = aws_acm_certificate.deck.arn
+#}
---
+++ config/clusters/kubernetes_outputs.tf
@@ -0,0 +1,3 @@
+output "deck_aws_acm_certificate_validation_options" {
+  value = aws_acm_certificate.deck.domain_validation_options
+}
