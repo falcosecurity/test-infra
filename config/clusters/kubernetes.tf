54c29566fd8838228cb378957fa01646aee2f29b
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2020-11-06 11:41:57 +0100

    chore(config/clusers/kubernetes/deck): attach the cert to the svc lb listener from k8s
    cleanup commented code
    
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/kubernetes.tf
+++ config/clusters/kubernetes.tf
@@ -1,19 +1,3 @@
-data "kubernetes_service" "deck" {
-  metadata {
-    name      = "deck"
-    namespace = "default"
-  }
-}
-
-#data "aws_lb" "deck" {
-#  name = data.kubernetes_service.deck.load_balancer_ingress[0].hostname
-#}
-
-#data "aws_lb_listener" "deck" {
-#  load_balancer_arn = data.aws_lb.deck.arn
-#  port              = 9090
-#}
-
 resource "aws_acm_certificate" "deck" {
   domain_name       = "prow.falco.org"
   validation_method = "DNS"
@@ -22,9 +6,3 @@ resource "aws_acm_certificate" "deck" {
     create_before_destroy = true
   }
 }
-
-#resource "aws_lb_listener_certificate" "deck" {
-#  #listener_arn    = data.aws_lb_listener.deck.arn
-#  listener_arn    = ""
-#  certificate_arn = aws_acm_certificate.deck.arn
-#}
