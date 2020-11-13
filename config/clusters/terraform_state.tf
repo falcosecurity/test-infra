ab482911ee715b6a29b0bdf3123bd5ba3ecd2574
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2020-11-13 19:57:39 +0100

    revert(config/clusters/state): disable logging on state access
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/terraform_state.tf
+++ config/clusters/terraform_state.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "falco-test-infra-state" {
     prevent_destroy = false
   }
 
-  logging {
-    target_bucket = aws_s3_bucket.falco-test-infra-state-logs.id
-    target_prefix = "log/"
-  }
-
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -32,16 +27,6 @@ resource "aws_kms_key" "falco-test-infra-state" {
   description             = "Falco Test Infra state master encryption key"
   deletion_window_in_days = 10
   enable_key_rotation     = true
-}
-
-resource "aws_s3_bucket" "falco-test-infra-state-logs" {
-  bucket = "falco-test-infra-state-logs"
-
-  acl = "log-delivery-write"
-
-  versioning {
-    enabled = true
-  }
 }
 
 resource "aws_dynamodb_table" "falco-test-infra-state-lock" {
