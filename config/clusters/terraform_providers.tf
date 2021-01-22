d9e1124c1e50e6fd46f8ec692b411662810a440b
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-01-22 17:18:09 +0100

    fix(config/clusters/kubernetes): remove unsupported provider argument
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/terraform_providers.tf
+++ config/clusters/terraform_providers.tf
@@ -22,7 +22,6 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  load_config_file       = "false"
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
