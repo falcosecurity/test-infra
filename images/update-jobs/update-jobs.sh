f031167cd405cf3f17edc0a2f7452fa076e16b3d
Leonardo Di Donato <leodidonato@gmail.com>
2021-01-22 13:19:31 +0100

    fix(images/update-jobs): the update-jobs go binary doesn't use dep anymore
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- images/update-jobs/update-jobs.sh
+++ images/update-jobs/update-jobs.sh
@@ -2,18 +2,14 @@
 set -eo pipefail
 set -o errexit
 
-DISABLE_MD_LINTING=1
-DISABLE_MD_LINK_CHECK=1
-export GO111MODULE=off
-
 export PULL_PULL_SHA=$PULL_PULL_SHA
 
 echo "******************************************************"
-echo "DryRun of updating job configs for github PR hash $PULL_PULL_SHA"
+echo "DryRun of updating job configs for GitHub PR hash $PULL_PULL_SHA"
 echo "******************************************************"
 
 # Using prowjob elements:
-# decorate: true 
+# decorate: true
 # Will pull in the PR git HASH into the image via the Initupload Sidecar
 
 export GOPATH="/home/prow/go"
@@ -21,7 +17,11 @@ export KUBECONFIG="/root/.kube/config"
 
 cd /home/prow/go/src/github.com/falcosecurity/test-infra/prow/update-jobs
 
-go get -u github.com/golang/dep/cmd/dep
+echo "******************************************************"
+echo "Building update-jobs"
+echo "******************************************************"
+
+make build
 
 echo "******************************************************"
 echo "Adding EKS value to kubeconfig"
@@ -30,12 +30,10 @@ echo "******************************************************"
 aws eks --region eu-west-1 update-kubeconfig --name falco-prow-test-infra
 
 echo "******************************************************"
-echo "Updating Go Dep and running job update"
+echo "Running job update"
 echo "******************************************************"
 
-dep ensure
-
-go run main.go --kubeconfig $KUBECONFIG --jobs-config-path /home/prow/go/src/github.com/falcosecurity/test-infra/config/jobs
+bin/update-jobs --kubeconfig $KUBECONFIG --jobs-config-path /home/prow/go/src/github.com/falcosecurity/test-infra/config/jobs
 
 echo "******************************************************"
 echo "Updated-jobs image"
