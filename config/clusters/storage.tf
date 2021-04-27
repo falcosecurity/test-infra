2b514f7db7b044f455af1a1c300bc64bbc9847e5
jonahjon <jonahjones094@gmail.com>
2021-04-27 11:05:15 -0500

    trying to fix lifecycle rule
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- config/clusters/storage.tf
+++ config/clusters/storage.tf
@@ -3,23 +3,23 @@ resource "aws_s3_bucket" "prow_storage" {
 
   acl = "private"
 
-  lifecycle_rule = [{
+  lifecycle_rule = {
     id      = "ten_day_retention_logs"
     prefix  = "logs/"
     enabled = true
     expiration = {
       days = 10
     }
-    },
-    {
+  }
+
+  lifecycle_rule = {
       id      = "ten_day_retention_pr_logs"
       prefix  = "pr-logs/"
       enabled = true
       expiration = {
         days = 10
       }
-    }
-  ]
+  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
