ba73f80a70f063d7369f81c3c6d9c3cb2c3b848c
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-05-07 16:21:09 +0200

    fix(config/clusters/ecr.tf): configure encryption at rest with managed kms keys
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/ecr.tf
+++ config/clusters/ecr.tf
@@ -1,31 +1,55 @@
 resource "aws_ecr_repository" "update_jobs" {
   name = "test-infra/update-jobs"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "golang" {
   name = "test-infra/golang"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "build_drivers" {
   name = "test-infra/build-drivers"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "docker_dind" {
   name = "test-infra/docker-dind"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "autobump" {
   name = "test-infra/autobump"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "arm_build" {
   name = "test-infra/arm-build"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "build_libs" {
   name = "test-infra/build-libs"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
 
 resource "aws_ecr_repository" "update_deployment_files" {
   name = "test-infra/update-falco-k8s-manifests"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 }
