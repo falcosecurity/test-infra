da666f38d17f9789f5e3a808cab0608ed466bf96
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-06-16 21:07:04 +0200

    fix(config/clusters): remove version constraints syntax deprecations
    Signed-off-by: maxgio92 <massimiliano.giovagnoli.1992@gmail.com>

--- config/clusters/terraform_providers.tf
+++ config/clusters/terraform_providers.tf
@@ -1,17 +1,13 @@
 provider "random" {
-  version = "~> 2.1"
 }
 
 provider "local" {
-  version = "~> 1.2"
 }
 
 provider "null" {
-  version = "~> 2.1"
 }
 
 provider "template" {
-  version = "~> 2.1"
 }
 
 provider "aws" {
--- config/clusters/terraform_versions.tf
+++ config/clusters/terraform_versions.tf
@@ -6,9 +6,22 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0.1"
     }
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.45.0"
+    }
+
+    random = {
+      version = "~> 2.1"
+    }
+
+    local = {
+      version = "~> 1.2"
+    }
+
+    null = {
+      version = "~> 2.1"
     }
   }
 }
