fe5203d4a84118e2202229bd5f9d2b5aa2b93218
Massimiliano Giovagnoli <me@maxgio.it>
2022-07-06 00:03:05 +0200

    refactor(images): apply dry to build and publish scripts
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- images/publish.sh
+++ images/publish.sh
@@ -2,62 +2,7 @@
 
 set -e
 
-usage () {
-    echo "Usage: \$ ${BASH_SOURCE[1]} /path/to/image"
-    exit 1
-}
-
-function start_docker() {
-    echo "Docker in Docker enabled, initializing..."
-    printf '=%.0s' {1..80}; echo
-    # If we have opted in to docker in docker, start the docker daemon,
-    service docker start
-    # the service can be started but the docker socket not ready, wait for ready
-    local WAIT_N=0
-    local MAX_WAIT=20
-    while true; do
-        # docker ps -q should only work if the daemon is ready
-        docker ps -q > /dev/null 2>&1 && break
-        if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
-            WAIT_N=$((WAIT_N+1))
-            echo "Waiting for docker to be ready, sleeping for ${WAIT_N} seconds."
-            sleep ${WAIT_N}
-        else
-            echo "Reached maximum attempts, not waiting any longer..."
-            exit 1
-        fi
-    done
-    printf '=%.0s' {1..80}; echo
-    echo "Done setting up docker in docker."
-}
-
-function aws_auth() {
-    echo "Logging into AWS ECR..."
-    printf '=%.0s' {1..80}; echo
-    # If we have opted in to docker in docker, start the docker daemon,
-    aws ecr get-login-password --region eu-west-1 \
-        | docker login \
-            --password-stdin \
-            --username AWS \
-            292999226676.dkr.ecr.eu-west-1.amazonaws.com
-    local WAIT_N=0
-    local MAX_WAIT=5
-    while true; do
-        # aws ecr describe image should only work if the auth is working
-        aws ecr describe-images --repository-name test-infra/docker-dind --max-items 1 > /dev/null 2>&1 && break
-        if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
-            WAIT_N=$((WAIT_N+1))
-            echo "Waiting for aws auth to work to be ready, sleeping for ${WAIT_N} seconds."
-            sleep ${WAIT_N}
-        else
-            echo "Reached maximum attempts, not waiting any longer..."
-            exit 1
-        fi
-    done
-    printf '=%.0s' {1..80}; echo
-    echo "Done setting up aws ecr auth"
-}
-
+source utils.sh
 
 start_docker
 
