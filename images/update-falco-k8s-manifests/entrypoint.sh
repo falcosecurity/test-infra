377c4639aa69f214a32b2237fab23f01a2273074
Leonardo Grasso <me@leonardograsso.com>
2022-08-30 14:55:54 +0200

    chore: correct old refs to evolution
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- images/update-falco-k8s-manifests/entrypoint.sh
+++ images/update-falco-k8s-manifests/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2021 The Falco Authors.
+# Copyright (C) 2022 The Falco Authors.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -142,7 +142,7 @@ function check_program {
     fi
 }
 
-# Meant to be run in the https://github.com/falcosecurity/evolution repository.
+# Meant to be run in the https://github.com/falcosecurity/deploy-kubernetes repository.
 # $1: path of the file containing the token
 main() {
     # Checks
