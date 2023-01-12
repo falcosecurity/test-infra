63372807bb2776d8104698a68e7937218eb7f119
Massimiliano Giovagnoli <me@maxgio.it>
2023-01-12 18:41:50 +0100

    deps(config/clusters/tf): upgrade terraform to latest stable
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- config/clusters/terraform_versions.tf
+++ config/clusters/terraform_versions.tf
@@ -1,27 +1,27 @@
 terraform {
-  required_version = "0.14.11"
+  required_version = "1.3.7"
 
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0.1"
+      version = "> 2.0.1"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45.0"
+      version = "> 3.45.0"
     }
 
     random = {
-      version = "~> 2.1"
+      version = "> 2.1"
     }
 
     local = {
-      version = "~> 1.2"
+      version = "> 1.2"
     }
 
     null = {
-      version = "~> 2.1"
+      version = "> 2.1"
     }
   }
 }
