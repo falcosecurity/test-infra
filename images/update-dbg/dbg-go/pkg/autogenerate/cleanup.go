0e87a45bc266a01e978c8def1bc61ab80de7807d
Federico Di Pierro <nierro92@gmail.com>
2023-08-03 15:28:22 +0200

    update(images): bump go image to golang:1.20.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

---
+++ images/update-dbg/dbg-go/pkg/autogenerate/cleanup.go
@@ -0,0 +1,24 @@
+package autogenerate
+
+import (
+	"fmt"
+	logger "github.com/sirupsen/logrus"
+	"os"
+)
+
+func cleanupExisting(opts Options) error {
+	logger.WithField("cmd", "autogenerate").Info("cleaning up existing config files")
+	for _, driverVersion := range opts.DriverVersion {
+		configPath := fmt.Sprintf(configPathFmt,
+			opts.RepoRoot,
+			driverVersion,
+			opts.Architecture,
+			"")
+		logger.WithField("cmd", "autogenerate").Debugf("removing folder: %s", configPath)
+		err := os.RemoveAll(configPath)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
