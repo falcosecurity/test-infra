a46113b6c9e5f7487cad99714abdb07723e4ab3d
Federico Di Pierro <nierro92@gmail.com>
2023-08-23 16:12:58 +0200

    cleanup(pkg): properly support dry-run in autogenerate command.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- images/update-dbg/dbg-go/pkg/autogenerate/cleanup.go
+++ images/update-dbg/dbg-go/pkg/autogenerate/cleanup.go
@@ -8,6 +8,10 @@ import (
 
 func cleanupExisting(opts Options) error {
 	logger.WithField("cmd", "autogenerate").Info("cleaning up existing config files")
+	if opts.DryRun {
+		logger.WithField("cmd", "autogenerate").Info("skipping because of dry-run.")
+		return nil
+	}
 	for _, driverVersion := range opts.DriverVersion {
 		configPath := fmt.Sprintf(configPathFmt,
 			opts.RepoRoot,
