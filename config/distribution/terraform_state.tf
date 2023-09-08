c4910198f5286c9ed0291dc397aaa22b46d1b764
Luca Guerra <luca@guerra.sh>
2023-09-08 15:14:49 +0000

    fix(tf): update bucket definition to be consistent with AWS state
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- config/distribution/terraform_state.tf
+++ config/distribution/terraform_state.tf
@@ -6,6 +6,16 @@ resource "aws_s3_bucket" "falco-distribution-state-bucket" {
   lifecycle {
     prevent_destroy = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = false
+      
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_dynamodb_table" "falco-distribution-state-bucket-lock" {
