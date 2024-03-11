7c4016d2f08fa69913380ff088e7b3136fe86e1b
Luca Guerra <luca@guerra.sh>
2024-03-11 09:29:54 +0000

    new(config): create distribution for play.falco.org
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- config/distribution/variables.tf
+++ config/distribution/variables.tf
@@ -22,3 +22,18 @@ variable "distribution_name_alias" {
   type    = string
   default = "download.falco.org"
 }
+
+variable "playground_bucket_name" {
+  type = string
+  default = "falco-playground"
+}
+
+variable "playground_name_alias" {
+  type = string
+  default = "play.falco.org"
+}
+
+variable "playground_origin_id" {
+  type = string
+  default = "falcoPlaygroundOrigin"
+}
