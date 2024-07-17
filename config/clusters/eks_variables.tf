7a96f3711eb2ec4b5b6d7224127668a2c9661d0c
Aldo Lacuku <aldo@lacuku.eu>
2024-07-17 15:58:48 +0200

    update(cluster): bump k8s version to 1.28
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- config/clusters/eks_variables.tf
+++ config/clusters/eks_variables.tf
@@ -1,5 +1,5 @@
 variable "eks_cluster_version" {
-  default     = "1.27"
+  default     = "1.28"
   description = "See https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html"
 }
 
