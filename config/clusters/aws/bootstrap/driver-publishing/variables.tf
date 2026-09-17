32468be46b9fed8769484c2c5013a5704ea9ea1c
c2ndev <cannarella.dev@gmail.com>
2026-09-17 16:15:08 +0200

    fix(ci): isolate manual AWS bootstrap from cluster workflows
    Signed-off-by: c2ndev <cannarella.dev@gmail.com>

--- config/clusters/aws/bootstrap/driver-publishing/variables.tf
+++ config/clusters/aws/bootstrap/driver-publishing/variables.tf
@@ -4,7 +4,7 @@ variable "aws_account_id" {
 
   validation {
     condition     = can(regex("^[0-9]{12}$", var.aws_account_id))
-    error_message = "aws_account_id must contain exactly 12 digits."
+    error_message = "The AWS account ID must contain exactly 12 digits."
   }
 }
 
